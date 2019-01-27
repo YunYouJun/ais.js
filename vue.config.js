e6b61e8398b93366f319e6aa4dd5d8acf25fe062
云游君 <me@yunyoujun.cn>
2019-01-28 00:24:22 +0800

    use circleci

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-function resolve (dir) {
-    return path.join(__dirname, dir)
-}
-module.exports = {
-  chainWebpack: config => {
-    config.resolve.alias.set('@', resolve('src'))
-  }
-}
