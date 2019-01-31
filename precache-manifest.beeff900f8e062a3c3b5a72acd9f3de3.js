e6c2dfd76f50ee97bde36b0a8927a3b9e935154a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-31 07:40:07 +0000

    Deploy YunYouJun/ais.js to github.com/YunYouJun/ais.js.git:gh-pages

---
+++ precache-manifest.beeff900f8e062a3c3b5a72acd9f3de3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "80e190c046f21560bd01",
+    "url": "js/chunk-vendors.6a261ca4.js"
+  },
+  {
+    "revision": "3c6d96a38100640f5a3f",
+    "url": "js/app.70ed45b5.js"
+  },
+  {
+    "revision": "46eadd26e2c65769e98a2e70c62a379c",
+    "url": "index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "80e190c046f21560bd01",
+    "url": "css/chunk-vendors.15d267b1.css"
+  },
+  {
+    "revision": "3c6d96a38100640f5a3f",
+    "url": "css/app.a8b8a1a9.css"
+  },
+  {
+    "revision": "28ff5f61fbf2ae633c742257ec255da6",
+    "url": "CNAME"
+  }
+];
